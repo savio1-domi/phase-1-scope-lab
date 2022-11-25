e8a498bc9615ec58b60b0648e70359ec32978e43
git-savio1.domi <techsavvydominic3@gmail.com>
2022-11-25 14:10:17 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+
+
+var customerName = 'bob'
+
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+
+
+const leastFavoriteCustomer = 'michelle'
+
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Michael'
+}
