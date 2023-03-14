0933a6b07c818ce912b2426aa6a82d4a188667f8
git-savio1.domi <techsavvydominic3@gmail.com>
2023-03-14 11:26:26 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
 // Write your solution in this file!
-
-
+// declaring a variable
 var customerName = 'bob'
 
-
 function upperCaseCustomerName() {
+   // modifying bob 
     return customerName = customerName.toUpperCase();
 }
 
-
+// declaring a variable
 const leastFavoriteCustomer = 'michelle'
-
 
 function setBestCustomer(){
     bestCustomer = 'not bob'
 }
 
-
 function overwriteBestCustomer(){
     bestCustomer = 'maybe bob'
 }
-
 
 function changeLeastFavoriteCustomer(){
     leastFavoriteCustomer = 'Michael'
